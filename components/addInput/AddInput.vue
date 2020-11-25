00454bd6ab028d291d716e0e2e8dba61b54af8bf
1797686126@qq.com <246A85D79TDAjin>
2020-11-25 13:03:33 +0800

    城市周边

---
+++ components/addInput/AddInput.vue
@@ -0,0 +1,69 @@
+<template>
+  <view class="fl jc-between add-input">
+    <input
+      class="input"
+      placeholder-class="placeholder-class"
+      type="text"
+      :placeholder="placeholder"
+      v-model="value"
+    />
+    <view class="btn" @click="add">{{ btn }}</view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    btn: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      default: "填写信息",
+    },
+  },
+  data() {
+    return {
+      value: "",
+    };
+  },
+  methods: {
+    add() {
+      this.$emit('add', this.value)
+    },
+    cleanVal() {
+        this.value = ''
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.add-input {
+  .input {
+    width: 436rpx;
+    height: 80rpx;
+    background: #f6f6f6;
+    border-radius: 40rpx;
+  }
+  .placeholder-class {
+    font-size: 20rpx;
+    color: #999999;
+    // line-height: 28rpx;
+    margin-left: 53rpx;
+  }
+
+  .btn {
+    width: 153rpx;
+    height: 80rpx;
+    background: #fe4555;
+    border-radius: 40rpx;
+
+    font-size: 20rpx;
+    color: #ffffff;
+    line-height: 80rpx;
+    text-align: center;
+  }
+}
+</style>
---
+++ pages/citySurrounding/JobHunting.vue
@@ -0,0 +1,177 @@
+<template>
+	<view class="job-hunting">
+		<view v-for="(item, index) in 3" :key="index" class="list">
+			<!-- 用户头部信息 -->
+			<view class="user-title">
+				<UserTitle top></UserTitle>
+			</view>
+			<view class="list-item">
+				<view class="comment-content">
+					求职信息文字求职信息文字求职信息文字求职信息文字求职信息文字求职信息文字求职信息文字
+				</view>
+				<view class="fl jc-between fl-warp comment-pics">
+					<image src="/static/img/house.png" mode="" v-for="(item, pic) in 5" :key="pic" class="fl jc-between fl-warp"></image>
+				</view>
+
+				<view class="access">326人浏览</view>
+
+				<!-- 评论 -->
+				<AddInput btn="留下信息" class="add-comment" @add="add"/>
+
+				<!-- 评论内容 -->
+				<view class="comment-list" >
+					<view class="comment-icon">
+						<image src="/static/img/comment.png" mode=""></image>评论
+					</view>
+
+					<view v-for="i in 3" :key="i">
+						<view class="fl jc-between al-center user-info">
+							<view class="header-name">
+								<image src="/static/img/house.png" mode=""></image>用户名
+							</view>
+						
+							<view class="publish-time">2020.07.30</view>
+						</view>
+											
+						<view class="content">求联系，电话11111111111</view>
+						
+					</view>
+					
+					
+				</view>
+
+				
+
+			</view>
+
+			<view class="null"></view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import UserTitle from "./UserTitle.vue";
+	import AddInput from "@/components/addInput/AddInput.vue"
+	export default {
+		components: {
+			UserTitle,AddInput
+		},
+		methods: {
+			add(val) {
+				console.log(val)
+			}
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+	.job-hunting {
+		background: #fff;
+		padding: 36rpx 40rpx 0 40rpx;
+
+		.list {
+			.user-title{
+				margin-top: 27rpx;
+			}
+
+			.list-item {
+				margin-top: 23rpx;
+		
+				// background: pink;
+			
+
+				.comment-content {
+					width: 599rpx;
+					font-size: 20rpx;
+					font-weight: 400;
+					color: #2e2e2e;
+					line-height: 28rpx;
+					margin: 31rpx auto;
+				}
+
+				.comment-pics {
+					width: 599rpx;
+					margin: 0 auto;
+
+					&::after {
+						content: "";
+						width: 175rpx;
+					}
+
+					image {
+						width: 175rpx;
+						height: 175rpx;
+						margin-bottom: 31rpx;
+					}
+				}
+
+				.access {
+					font-size: 16rpx;
+					font-weight: 400;
+					color: #646464;
+					width: 599rpx;
+					margin: 0 auto;
+				}
+				.add-comment{
+					width: 599rpx;
+					margin: 0 auto;
+				}
+
+				.comment-list {
+					width: 595rpx; //  671px;
+					padding: 0 38rpx;
+					border-top: 2rpx solid #f6f6f6;
+					margin-top: 48rpx;
+
+					.comment-icon {
+						font-size: 20rpx;
+						font-weight: 400;
+						color: #2e2e2e;
+						margin: 18rpx 0 43rpx 0;
+
+						image {
+							width: 25rpx;
+							height: 17rpx;
+							object-fit: cover;
+							margin-right: 5rpx;
+						}
+					}
+
+					.user-info {
+						width: 596rpx;
+						font-size: 16rpx;
+						font-weight: 400;
+						color: #646464;
+						.header-name {
+							image {
+								width: 42rpx;
+								height: 42rpx;
+								border-radius: 50%;
+								vertical-align: middle;
+								margin-right: 15rpx;
+							}
+						}
+
+
+					}
+					
+					.content{
+						font-size: 16rpx;
+						color: #646464;
+						margin: 15rpx 0 0 0;
+						padding-bottom: 27rpx;
+						border-bottom: 2rpx solid #f6f6f6;
+					}
+				}
+			}
+		
+		.null{
+			width: 750rpx;
+			height: 14rpx;
+			background: #F6F6F6;
+			margin-left: -38rpx;
+		}
+		
+		}
+	}
+</style>
